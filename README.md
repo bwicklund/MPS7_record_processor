64d5c118c95f42cbe86788ec6c08342c23e624ab
Bryon Wicklund <bryon.wicklund@novu.com>
2017-10-22 14:15:57 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,84 @@
+# record_processor
+
+Script to parse a custom protocol format (MPS7) and answer some questions about the data provided.
+
+### Usage
+
+```bash
+$ cd <project_directory>
+$ record_processor.rb <data_file>
+```
+
+Examples:
+
+```bash
+$ cd /User/bryon.wicklund/projects/record_processor/
+$ record_processor.rb data.dat
+```
+
+### Currently supported functionality
+
+Reads data in following format:
+
+Header:
+```
+| 4 byte magic string "MPS7" | 1 byte version | 4 byte (uint32) # of records |
+```
+
+Row:
+```
+| 1 byte record type enum | 4 byte (uint32) Unix timestamp | 8 byte (uint64) user ID | 8 byte (float64) (if enum is 0 || 1)
+```
+
+Crude Validations:
+
+Program will `exit` if it does not detect the "magic string": 'MPS7'
+
+Program will `warn` if total records does not match the record count in header
+
+Program will `exit` if epoch time provided is not a integer
+
+Program will `exit` if user ID is not a valid unsigned 8byte Int
+
+Program will `exit` if dollar amount is not a valid float
+
+Program will `exit` if record type is not in the follow list:
+```
+0x00: Debit
+0x01: Credit
+0x02: StartAutopay
+0x03: EndAutopay
+```
+
+
+#### Header Row `unpack`
+```
+a4CN
+
+a4 = 4 byte arbitrary binary string
+C = unsigned 8-bit(integer)
+N = 32-bit unsigned, network (big-endian) byte order
+```
+#### Data Row `unpack`
+
+```
+CNQ>G
+
+C = unsigned 8-bit(integer)
+N = 32-bit unsigned, network (big-endian) byte order
+Q> = 64-bit unsigned, big-endian (uint64_t)
+G (optionally) = double-precision, network (big-endian) byte order (If enum is 0 or 1)
+```
+
+#### Results on data set given in `data.dat`
+```
+WARNING: Records count does not match header value: Records: 72 Header: 71
+Header Row Record Count: 71
+Total Records: 72
+Total Credits: $10073.36
+Total Debits: $18203.70
+Total Autopays Started: 10
+Total Autopays Ended: 8
+Balance for user ID 2456938384156277127: $0.00
+```
+
---
+++ record_processor.rb
@@ -0,0 +1,154 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+if ARGV[0].nil?
+  usage
+  exit(false)
+end
+FILE = ARGV[0].freeze
+
+# Header Row Variables
+MAGIC_STRING = 'MPS7'
+HEADER_SIZE = 9
+HEADER_FORMAT = 'a4CN'
+
+# Data Row Variables
+ENUM_FORMAT = 'C'
+BASE_DATA_LINE_FORMAT = 'NQ>'
+ADDTIONAL_DATA_LINE_FORMAT = 'G'
+RECORD_TYPE_ENUM = {
+  0 => 'Debit',
+  1 => 'Credit',
+  2 => 'StartAutopay',
+  3 => 'StopAutopay'
+}.freeze
+
+# Check to make sure file exists and is readable by user
+file_check(FILE)
+
+File.open(FILE, 'rb') do |f|
+  results = {
+    records: 0,
+    total_debits: 0,
+    total_credits: 0,
+    autopays_started: 0,
+    autopays_ended: 0,
+    header_record_count: 0,
+    balance_for_2456938384156277127: 0
+  }
+  # Header Row
+  process_header(f, results)
+  # Data Rows
+  process_data_rows(f, results)
+  # Output Results
+  print_results(results)
+end
+
+BEGIN {
+  def usage
+    puts 'usage: record_processor.rb data_file'
+  end
+
+  def file_check(file)
+    # Check to make sure file exists and is readable by user
+    # raise exception if not.
+    unless File.readable?(file) && File.exist?(file)
+      raise 'Either file does not exist or this user ' \
+            'does not have read permissions'
+    end
+  end
+
+  def process_header(file_handle, results)
+    # Extract header row
+    header_arr = file_handle.read(HEADER_SIZE).unpack(HEADER_FORMAT)
+
+    # Error out if we do not see Magic String (MPS7)
+    raise 'Invalid header format!' if header_arr.first != MAGIC_STRING
+
+    results[:header_record_count] = header_arr[2]
+  end
+
+  def process_data_rows(file_handle, results)
+    until file_handle.eof?
+      # Extract record type
+      type_enum = file_handle.read(1).unpack(ENUM_FORMAT).first
+      record_type = RECORD_TYPE_ENUM[type_enum]
+
+      # Raise exception if row contains unknown record type
+      raise 'Unknown Record Type!' if record_type.nil?
+
+      # Get format and bytes based on record type
+      bytes, unpack_format =
+        case [RECORD_TYPE_ENUM[0], RECORD_TYPE_ENUM[1]].include? record_type
+        when true
+          [20, BASE_DATA_LINE_FORMAT + ADDTIONAL_DATA_LINE_FORMAT]
+        when false
+          [12, BASE_DATA_LINE_FORMAT]
+        end
+
+      # Read row
+      data_row = [record_type, file_handle.read(bytes).unpack(unpack_format)]
+                 .flatten
+
+      validate_row_data(data_row)
+
+      add_data_row_to_results(data_row, results)
+    end
+  end
+
+  def validate_row_data(data_row)
+    # !!Crude!! validation of data
+    raise 'Invalid epoch time' unless /^\d+$/ =~ data_row[1].to_s
+
+    raise 'Invalid user ID' unless data_row[2] >= 0 &&
+                                   # Max unsigned 8byte Int
+                                   data_row[2] <= 18_446_744_073_709_551_615
+    begin
+      Float(data_row[3] || 0)
+    rescue
+      raise 'Invalid float'
+    end
+  end
+
+  # Tally row results
+  def add_data_row_to_results(data_row, results)
+    results[:records] += 1
+    case data_row[0] # Record Type
+    when 'Debit'
+      results[:total_debits] += data_row[3]
+      if data_row[2] == 2_456_938_384_156_277_127
+        results[:balance_for_2456938384156277127] += data_row[3]
+      end
+    when 'Credit'
+      results[:total_credits] += data_row[3]
+      if data_row[2] == 2_456_938_384_156_277_127
+        results[:balance_for_2456938384156277127] -= data_row[3]
+      end
+    when 'StartAutopay'
+      results[:autopays_started] += 1
+    when 'StopAutopay'
+      results[:autopays_ended] += 1
+    end
+  end
+
+  def print_results(results)
+    warn 'WARNING: Records count does not match header value: ' \
+      "Records: #{results[:records]} Header: #{results[:header_record_count]}"
+    puts
+    puts "Header Row Record Count: #{results[:header_record_count]}"
+    puts "Total Records: #{results[:records]}"
+    puts "Total Credits: $#{round_nicely(results[:total_credits])}"
+    puts "Total Debits: $#{round_nicely(results[:total_debits])}"
+    puts "Total Autopays Started: #{results[:autopays_started]}"
+    puts "Total Autopays Ended: #{results[:autopays_ended]}"
+    puts 'Balance for user ID 2456938384156277127: $'\
+      "#{round_nicely(results[:balance_for_2456938384156277127])}"
+  end
+
+  def round_nicely(num)
+    # Leave 0 at end example:
+    # 3.70000.round(2) // 3.7
+    # sprintf('%.2f',  3.70000) // 3.70
+    format('%.2f', num)
+  end
+}
